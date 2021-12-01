e4065b4b03c5d359c67aa32d7cffb5be8c2ea082
Filip Gabrys <el.filberto@gmail.com>
2021-12-01 12:03:28 +0100

    Add tests to check conditions for movement

--- src/main/java/com/testing/Main.java
+++ src/main/java/com/testing/Main.java
@@ -1,19 +1,23 @@
 package com.testing;
 
-import com.checkers.*;
+import com.checkers.board.Board;
+import com.checkers.figures.Figure;
+import com.checkers.figures.FigureColor;
+import com.checkers.figures.Pawn;
+import com.checkers.figures.Queen;
 
 public class Main {
     public static void main (String[] args) throws java.lang.Exception {
         Board newBoard = new Board();
-        Figure q1 = new Queen(FigureColor.WHITE);
-        Figure p1 = new Pawn(FigureColor.BLACK);
-        newBoard.setFigure(0, 0, q1);
-        newBoard.setFigure(4, 7, p1);
-        newBoard.setFigure(5, 6, new Pawn(FigureColor.WHITE));
+        newBoard.setFigure(1, 0, new Pawn(FigureColor.BLACK));
+        newBoard.setFigure(4, 7, new Pawn(FigureColor.WHITE));
+        newBoard.setFigure(5, 6, new Pawn(FigureColor.BLACK));
         System.out.println(newBoard);
-        newBoard.move(4, 7,  5, 6);
+        newBoard.move(1, 0, 0, 2);
         System.out.println(newBoard);
-        newBoard.move(6, 5, 7, 4);
+        newBoard.move(4, 7,  6, 5);
+        System.out.println(newBoard);
+
         System.out.println(newBoard);
     }
 }
--- src/test/java/com/checkers/testing/CheckersTestSuite.java
+++ src/test/java/com/checkers/testing/CheckersTestSuite.java
@@ -1,7 +1,60 @@
 package com.checkers.testing;
 
+import com.checkers.board.Board;
+import com.checkers.figures.FigureColor;
+import com.checkers.figures.Pawn;
 import org.junit.jupiter.api.*;
-import com.checkers.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Checkers Test Suite")
 public class CheckersTestSuite {
+    @BeforeEach
+    public void before() {
+        System.out.println("Test stage: begin");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test stage: complete");
+    }
+
+    @DisplayName("testing player pick")
+    @Test
+    void testBooleanCheckPlayerPick() {
+        //Given
+        Board newBoard = new Board();
+        newBoard.setFigure(4, 1, new Pawn(FigureColor.BLACK));
+        boolean result = true;
+        FigureColor whoseMove = FigureColor.BLACK;
+
+        //When
+        boolean firstPick = newBoard.isInRange(4, 1);
+        boolean isThereAFigure = newBoard.isFigurePresent(4, 1);
+        boolean colorRound = newBoard.checkColor(4,1);
+        boolean tileColor = newBoard.checkTileColor(4, 2);
+
+        //Then
+        assertTrue(firstPick);
+        assertTrue(isThereAFigure);
+        //assertFalse(colorRound);
+        assertFalse(tileColor);
+        assertFalse(newBoard.checkPlayerPick(4, 1, 5, 2, result));
+        assertFalse(newBoard.move(4,1, 5, 3));
+    }
+
+    /*@Test
+    void testPlayerRoundSwitch() {
+        //Given
+        Board newBoard = new Board();
+        newBoard.setFigure(4, 1, new Pawn(FigureColor.WHITE));
+        boolean result = true;
+        FigureColor whoseMove = FigureColor.WHITE;
+
+        //When
+        newBoard.move(4, 1, 5, 2);
+
+        //Then
+        assertEquals(FigureColor.BLACK, whoseMove);
+    }*/
 }
