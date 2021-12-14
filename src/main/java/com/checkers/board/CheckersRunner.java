faf6a3c77919e63b14271a92956912a21c582abb
Filip Gabrys <el.filberto@gmail.com>
2021-12-14 21:45:13 +0100

    Fix the missing javafx files

---
+++ src/main/java/com/checkers/board/CheckersRunner.java
@@ -0,0 +1,2 @@
+package com.checkers.board;public class CheckersRunner {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module Checkers.main {
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+
+    opens com.checkers.board;
+}
