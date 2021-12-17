0c44a4fe6b416f1ad4fb932dcd771302a574d11e
Filip Gabrys <el.filberto@gmail.com>
2021-12-17 19:06:25 +0100

    Transfer display methods to Game class

--- src/main/java/com/checkers/board/CheckersRunner.java
+++ src/main/java/com/checkers/board/CheckersRunner.java
@@ -1,26 +1,24 @@
 package com.checkers.board;
 
+import com.checkers.Game;
+import  com.checkers.board.Board;
 import com.checkers.figures.FigureColor;
 import com.checkers.figures.Pawn;
 import com.checkers.figures.Queen;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.*;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class CheckersRunner extends Application {
 
     private final Image checkBoard = new Image("file:src/main/resources/board.jpg");
-    private final Image blackPawn = new Image("file:src/main/resources/Figures/blackPawn.gif");
-    private final Image whitePawn = new Image("file:src/main/resources/Figures/whitePawn.gif");
-    private final Image blackQueen = new Image("file:src/main/resources/Figures/blackQueen.gif");
-    private final Image whiteQueen = new Image("file:src/main/resources/Figures/whiteQueen.gif");
     private final Image newGame = new Image("file:src/main/resources/Menu/newGame.gif");
     private final Image exitGame = new Image("file:src/main/resources/Menu/exit.gif");
     private final FlowPane menuButtons = new FlowPane(Orientation.VERTICAL);
@@ -31,7 +29,7 @@ public class CheckersRunner extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane grid = showMenu();
+        GridPane grid = showMenu(primaryStage);
         showScene(primaryStage, grid);
     }
 
@@ -42,43 +40,36 @@ public class CheckersRunner extends Application {
         primaryStage.show();
     }
 
-    private GridPane showMenu() {
+    private GridPane showMenu(Stage primaryStage) {
         Background background = setBackgroundBoard(checkBoard);
         GridPane grid = createMenuPane(background);
-        setMenuButton(grid);
+        setMenuButton(grid, primaryStage);
         return grid;
-
     }
 
-    private void setMenuButton(GridPane grid) {
-        setStartbutton();
+    private void setMenuButton(GridPane grid, Stage stage) {
+        setStartbutton(stage);
+        menuButtons.setVgap(20);
         setExitButton();
         menuButtons.setAlignment(Pos.CENTER);
-        grid.add(menuButtons,0, 0);
+        grid.add(menuButtons, 0, 0);
     }
 
     private void setExitButton() {
         Button treminateGame = new Button();
         treminateGame.setGraphic(new ImageView(exitGame));
-        treminateGame.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.exit(0);
-            }
-        });
+        treminateGame.setBackground(null);
+        treminateGame.setOnAction(event -> System.exit(0));
         menuButtons.getChildren().add(treminateGame);
     }
 
-    private void setStartbutton() {
+    private void setStartbutton(Stage stage) {
         Button startNewGame = new Button();
         startNewGame.setGraphic(new ImageView(newGame));
-        startNewGame.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                Stage primaryStage = new Stage();
-                GridPane grid = startANewGame();
-                showScene(primaryStage, grid);
-            }
+        startNewGame.setBackground(null);
+        startNewGame.setOnAction(event -> {
+            GridPane grid = startANewGame();
+            showScene(stage, grid);
         });
         menuButtons.getChildren().add(startNewGame);
     }
@@ -88,80 +79,29 @@ public class CheckersRunner extends Application {
         GridPane grid = createGridPane(background);
         Board newBoard = new Board();
         newBoard.setNewGame();
-        displayOnBoard(grid, newBoard);
+        Game game = new Game(grid, newBoard);
+        game.displayOnBoard();
+        grid.setOnMouseClicked(e -> {
+            int x = (int)e.getX()/100;
+            int y = (int)e.getY()/100;
+            game.doClick(x, y);
+        });
         return grid;
     }
 
     private Background setBackgroundBoard(Image image) {
-        BackgroundSize backgroundSize = new BackgroundSize(100,100, true, true, true, false);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
         BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
         return new Background(backgroundImage);
-    }
-
-    private void displayOnBoard(GridPane grid, Board board) {
-        for (int row = 0; row <= 7; row++) {
-            for (int col = 0; col <= 7; col++) {
-                if (board.getFigure(col, row) instanceof Pawn)
-                    displayPawn(grid, board, row, col);
-                else if (board.getFigure(col, row) instanceof Queen)
-                    displayQueen(grid, board, row, col);
-            }
-        }
-    }
-
-    private void displayQueen(GridPane grid, Board board, int row, int col) {
-        if ((board.getFigure(col, row).getColor().equals(FigureColor.WHITE))) {
-            addWhiteQueen(grid, col, row);
-        } else if ((board.getFigure(col, row).getColor().equals(FigureColor.BLACK))) {
-            addBlackQueen(grid, col, row);
-        }
-    }
-
-    private void displayPawn(GridPane grid, Board board, int row, int col) {
-        if ((board.getFigure(col, row).getColor().equals(FigureColor.WHITE))) {
-            addBWhitePawn(grid, col, row);
-        } else if ((board.getFigure(col, row).getColor().equals(FigureColor.BLACK))) {
-            addBlackPawn(grid, col, row);
-        }
-    }
-
-    private void addBlackQueen(GridPane grid, int col, int row) {
-        ImageView queen = new ImageView(blackQueen);
-        figureDisplayAlignment(queen);
-        grid.add(queen, col, row);
-    }
-
-    private void addWhiteQueen(GridPane grid, int col, int row) {
-        ImageView queen = new ImageView(whiteQueen);
-        figureDisplayAlignment(queen);
-        grid.add(queen, col, row);
-    }
-
-    private void addBlackPawn(GridPane grid, int col, int row) {
-        ImageView pawn = new ImageView(blackPawn);
-        figureDisplayAlignment(pawn);
-        grid.add(pawn, col, row);
-    }
-
-    private void addBWhitePawn(GridPane grid, int col, int row) {
-        ImageView pawn = new ImageView(whitePawn);
-        figureDisplayAlignment(pawn);
-        grid.add(pawn, col, row);
-    }
-
-    private void figureDisplayAlignment(ImageView figure) {
-        GridPane.setHalignment(figure, HPos.CENTER);
-        GridPane.setValignment(figure, VPos.CENTER);
     }
 
     private GridPane createGridPane(Background background) {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setBackground(background);
-        createRows(grid, 8);
-        createColumns(grid, 8);
+        createRows(grid);
+        createColumns(grid);
         grid.setPadding(new Insets(-15, -15, -15, -15));
-        grid.setGridLinesVisible(true);
         return grid;
     }
 
@@ -169,24 +109,23 @@ public class CheckersRunner extends Application {
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setBackground(background);
-        grid.setPadding(new Insets(-15, -15, -15, -15));
         return grid;
     }
 
-    private void createColumns(GridPane grid, int col) {
+    private void createColumns(GridPane grid) {
         ColumnConstraints cc = new ColumnConstraints();
-        cc.setPercentWidth(100 / col);
+        cc.setPercentWidth(100 / 8);
 
-        for (int i = 0; i < col; i++) {
+        for (int i = 0; i < 8; i++) {
             grid.getColumnConstraints().add(cc);
         }
     }
 
-    private void createRows(GridPane grid, int row) {
+    private void createRows(GridPane grid) {
         RowConstraints rc = new RowConstraints();
-        rc.setPercentHeight(100 / row);
+        rc.setPercentHeight(100 / 8);
 
-        for (int i = 0; i < row; i++) {
+        for (int i = 0; i < 8; i++) {
             grid.getRowConstraints().add(rc);
         }
     }
